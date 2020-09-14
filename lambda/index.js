460cd64fb616dded95f29f67c3e3a177d72da9c6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-14 15:45:17 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -58,7 +58,7 @@ const postRequest = (data) => {
       .post(url,data)
       .then((res) => res.data)
       .then((res) => {
-        resolve(res.form.outputData);
+        resolve(res.outputData);
       })
       .catch((err) => {
         reject("", err);
